7a2b72a7e04bd94172702f2cd6129fa5c1d11195
mullergc <mullergabriel94@gmail.com>
2023-10-23 11:35:37 -0300

    OFF

--- R/read_table_pedidos.R
+++
@@ -1,7 +0,0 @@
-read_table_pedidos <- function(url_table,email){
-  googledrive::drive_auth(email = email)
-  googlesheets4::gs4_auth(token = googledrive::drive_token())
-
-  df <- googlesheets4::read_sheet(url,  encoding = "UTF8")
-
-}
