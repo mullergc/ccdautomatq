30cf4ca9fccd3a7eca243a9a6b7ebd0f4712af3e
mullergc <mullergabriel94@gmail.com>
2023-10-30 09:36:57 -0300

    TESTE_TIMESTAMP

--- R/run_script.R
+++ R/run_script.R
@@ -177,32 +177,3 @@ gw_query_test <- function(url_pedidos, credspath,periodo='Diário',status='TESTE
   cat("Todas as queries executadas!\n")
   print("Tempo de execução (minutos):", total_time_taken)
 }
-
-
-# Define your run_function
-gtw_query <- function(num_chamado,url_sql, url_sheets, credspath, date_change = FALSE,url_ds, sheetname = 'Pagina1') {
-  # Use tryCatch to handle errors
-  tryCatch({
-    # Read the JSON file as text
-    creds_text <- readLines(credspath, warn = FALSE)
-
-    # Parse the JSON text
-    creds <- jsonlite::fromJSON(creds_text)
-    # Extract the credentials
-    email <- creds$email
-    username <- creds$username
-    password <- creds$pass
-    dbname <- creds$dbname
-    sql <- read_sql(email = email, url = url_sql)
-    r <- get_query_auto(usernamedb = username, passwordb = password, dbname = dbname, query = sql)
-    write_query_sheet(df = r, url_destiny = url_sheets, date_change = date_change, sheetname)
-
-    # Success message
-    cat("Function executed successfully\n")
-  }, error = function(e) {
-    # Handle errors and log them
-    error_msg <- conditionMessage(e)
-    log_error(error_msg,num_chamado)
-    cat("Function encountered an error.See error_log please\n",num_chamado)
-  })
-}
